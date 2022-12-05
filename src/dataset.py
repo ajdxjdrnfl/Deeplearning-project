f0cf1ca3ee160e221b51d9912ed312729d6861ec
ajdxjdrnfl <imreal4023@naver.com>
2022-12-06 00:06:39 +0900

    training

--- src/dataset.py
+++ src/dataset.py
@@ -6,17 +6,19 @@ import torch.nn as nn
 from PIL import Image
 import os
 import numpy as np
-
+import random
 
 class MyDataSet(torch.utils.data.Dataset):
     def __init__(self, path, transform=None):
         self.path = path
 
-
         self.real_list = os.listdir(os.path.join(self.path, "real/"))
-        self.syn_list = os.listdir(os.path.join(self.path, "syn/"))
+        syn_list = os.listdir(os.path.join(self.path, "syn/"))
+        size = len(syn_list)
+        self.syn_list = random.sample(syn_list, int(size))
         self.class_list = [0]*len(self.real_list) + [1]*len(self.syn_list)
-        self.image_list = self.real_list + self.syn_list
+        self.image = self.real_list + self.syn_list
+        self.image_list = [file for file in self.image if file.endswith('.jpg')]
         
         self.transform = transform
 
